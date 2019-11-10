1717d3c7f7919f369bbc2a83164e70a7b3e4637e
7ropkillaz <kusainovdaniyar@gmail.com>
2019-11-10 21:29:52 +0400

    Create project.

---
+++ src/com/company/model/Restaurant.java
@@ -0,0 +1,32 @@
+package com.company.model;
+
+public class Restaurant {
+    private String address;
+    private String name;
+    private int tables;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTables() {
+        return tables;
+    }
+
+    public void setTables(int tables) {
+        this.tables = tables;
+    }
+}
---
+++ src/com/company/model/RestaurantRepository.java
@@ -0,0 +1,7 @@
+package com.company.model;
+
+public interface RestaurantRepository {
+    void edit();
+    Restaurant get();
+
+}
