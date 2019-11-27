f64c459145bb64c512cc643a865fe9f95c0c289a
7ropkillaz <kusainovdaniyar@gmail.com>
2019-11-27 16:32:38 +0400

    +

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,38 @@
 package com.company;
 
-import com.company.controller.RestaurantController;
 import com.company.model.Restaurant;
-import com.company.repository.RestaurantRepository;
-import com.company.repository.RestaurantRepositoryImpl;
-
+import com.company.repository.FileRepos;
 public class Main {
 
     public static void main(String[] args) {
-        RestaurantRepository restaurantRepository=new RestaurantRepositoryImpl();
-        RestaurantController rest=new RestaurantController(restaurantRepository);
-        rest.add("Dachnaya","Perchini1",4,"1333");
-
-        Restaurant[] restaurants=rest.getAll();
-        System.out.println("**"+restaurants[0].toString());
-        rest.edit("Dachnaya","PerchiniHall",3,"1333");
-        Restaurant[] restaurants1=rest.getAll();
-        System.out.println(restaurants1[0].getName());
-
-            rest.remove("1333");
-            Restaurant[] restaurants2 = rest.getAll();
-        if(restaurants2.length!=0) {
-            System.out.println(restaurants2[0].getName());
+    //    RestaurantRepository restaurantRepository=new RestaurantRepositoryImpl();
+    //    RestaurantController rest=new RestaurantController(restaurantRepository);
+    //    rest.add("Dachnaya","Perchini1",4,"1333");
+//
+    //    Restaurant[] restaurants=rest.getAll();
+    //    System.out.println("**"+restaurants[0].toString());
+    //    rest.edit("Dachnaya","PerchiniHall",3,"1333");
+    //    Restaurant[] restaurants1=rest.getAll();
+    //    System.out.println(restaurants1[0].getName());
+//
+    //        rest.remove("1333");
+    //        Restaurant[] restaurants2 = rest.getAll();
+    //    if(restaurants2.length!=0) {
+    //        System.out.println(restaurants2[0].getName());
+    //    }
+    //    else {
+    //        System.out.println("Массив равен нулю");
+    //    }
+        Restaurant r=new Restaurant("Dachnaya","Stud123",4,"1333");
+        Restaurant d=new Restaurant("Dachnaya2","Perchini",4,"13334");
+       FileRepos fileRepos=new FileRepos();
+       fileRepos.add(r);
+       fileRepos.add(d);
+       Restaurant[] rest=fileRepos.getAll();
+        if(rest.length!=0){
+            System.out.println(rest[0].getName());
         }
-        else {
-            System.out.println("Массив равен нулю");
-        }
-
+        fileRepos.remove("1333");
+        fileRepos.edit("WQeqwe","Perchini",4,"13334");
     }
 }
--- src/com/company/repository/FileRepos.java
+++ src/com/company/repository/FileRepos.java
@@ -2,29 +2,125 @@ package com.company.repository;
 
 import com.company.model.Restaurant;
 
-import java.io.Reader;
-import java.io.Writer;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FileRepos implements RestaurantRepository {
 
     @Override
     public void edit(String address, String name, int tables, String id) {
+        Restaurant[] restaurants = getAll();
+        ArrayList<Restaurant> rest = new ArrayList<>(Arrays.asList(restaurants));
+        Restaurant r;
+        try {
+            if (id != null) {
+                for (int i = 0; i < rest.size(); i++) {
+                    r = rest.get(i);
+
+                    if (r.getId().equals(id)) {
+                        r.setAddress(address);
+                        r.setName(name);
+                        r.setTables(tables);
+                        BufferedWriter bw = new BufferedWriter(new FileWriter("text.txt", false));
+                        bw.write("");
+                        bw.close();
+
+                    }
+
+                }
+            }
+            for (int i = 0; i < rest.size(); i++) {
+                r = rest.get(i);
+                add(r);
+
+
+            }
+        } catch (IOException e) {
+        }
+
 
     }
 
     @Override
     public Restaurant[] getAll() {
+        ArrayList<Restaurant> rest = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\kusai\\IdeaProjects\\restaurant-3\\text.txt"))) {
 
-        return null;
+            String s;
+            String address = "";
+            String name = "";
+            int tables = 0;
+            String id = "";
+            while ((s = br.readLine()) != null) {
+                address = new String(s);
+                if ((s = br.readLine()) != null) {
+                    name = new String(s);
+                }
+                if ((s = br.readLine()) != null) {
+                    tables = Integer.parseInt(s);
+                }
+                if ((s = br.readLine()) != null) {
+                    id = new String(s);
+                }
+                rest.add(new Restaurant(address, name, tables, id));
+            }
+        } catch (IOException e) {
+        }
+        return rest.toArray(new Restaurant[rest.size()]);
+
     }
 
     @Override
     public void remove(String id) {
+        FileRepos fileRepos = new FileRepos();
+        Restaurant[] restaurants = fileRepos.getAll();
+        ArrayList<Restaurant> rest = new ArrayList<>(Arrays.asList(restaurants));
+        Restaurant r;
+        try {
+            if (id != null) {
+                for (int i = 0; i < rest.size(); i++) {
+                    r = rest.get(i);
+
+                    if (r.getId().equals(id)) {
+                        rest.remove(i);
+                        BufferedWriter bw = new BufferedWriter(new FileWriter("text.txt", false));
+                        bw.write("");
+                        bw.close();
+
+                    }
+
+                }
+            }
+            for (int i = 0; i < rest.size(); i++) {
+                r = rest.get(i);
+                fileRepos.add(r);
+
+
+            }
+        } catch (IOException e) {
+        }
+
 
     }
 
     @Override
     public void add(Restaurant r) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\kusai\\IdeaProjects\\restaurant-3\\text.txt", true))) {
+
+
+            bw.write(r.getAddress());
+            bw.newLine();
+            bw.write(r.getName());
+            bw.newLine();
+            bw.write(Integer.toString(r.getTables()));
+            bw.newLine();
+            bw.write(r.getId());
+            bw.newLine();
+            bw.close();
+
+        } catch (IOException e) {
+        }
 
     }
 }
