d96c33ffe3a2a4e4e42ec80b8731e1b5a33236eb
gavrbot <asakurazik2@gmail.com>
2020-01-20 16:54:05 +0400

    Added DishDataBaseImpl

--- src/main/java/com/company/controller/ServletController.java
+++ src/main/java/com/company/controller/ServletController.java
@@ -2,42 +2,34 @@ package com.company.controller;
 
 import com.company.repository.DishDataBaseImpl;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ServletController extends HttpServlet {
     DishDataBaseImpl impl;
     ServletController(DishDataBaseImpl impl){
         this.impl = impl;
     }
-    public void get(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        int id=0;//здесь должен получаться каким=то образом id
-        PrintWriter writer = response.getWriter();
-        try {
-            writer.println(impl.get(id));
-        }
-        finally {
-            writer.close();
-        }
-    }
-    public void put(){
 
-    }
-    public void post(){
-
-    }
-    public void delete(){
-
-    }
-    public void head(){
-
-    }
-    public void options(){
-
+    public ServletController() {
+        super();
     }
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
 }
